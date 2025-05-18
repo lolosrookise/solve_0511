95ce7746b2b1b99c759ff37b6c8729ff7ab64709
lolosrookise <gifihil@gmail.com>
2025-05-18 14:34:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('a')
+a=input()
+b=a.split(' ')
+c=0
+while c!=-1:
+    for i in range(len(b)-1):
+        c=0
+        if len(b)>=1 and b[i] == b[i+1]:
+            b.pop(i)
+            b.pop(i+1)
+            c+=1
+            break
+    if c == 0:
+        c=-1
+
+if len(b) < 1:
+    print(1)
+else:
+    print(0)
+
