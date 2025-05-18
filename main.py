dffa92d587a3f187fd0591ac54258c7d86ddc161
lolosrookise <gifihil@gmail.com>
2025-05-18 21:00:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ a=input()
 b=a.split(' ')
 c=0
 while c!=-1:
-    for i in range(len(b)-1):
-        c=0
-        if len(b)>=1 and b[i] == b[i+1]:
+    c=0
+    for i in range(len(b)-1, 0, -1):
+        if len(b)>=1 and b[i] == b[i-1]:
             b.pop(i)
-            b.pop(i+1)
+            b.pop(i-1)
             c+=1
             break
     if c == 0:
@@ -14,6 +14,7 @@ while c!=-1:
 
 if len(b) < 1:
     print(1)
+
 else:
     print(0)
 
